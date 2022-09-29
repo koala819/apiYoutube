5f7c1d39332d99ddf4d097bd663397d6b024a8a0
koala819 <x.genolhac@gmail.com>
2022-09-29 13:29:57 +0200

    fix

--- server.js
+++ server.js
@@ -4,39 +4,19 @@ const {google} = require("googleapis");
 
 const app = express();
 const port = 3000;
-const apiKey = "AIzaSyDfcp_4XjC669APG2hmWWXlcYjwZuzlQDE";
+const apiKey = "AIzaSyCsfBmbTeLJGkkkv90-QivteF8PQ7pytqY";
 const apiUrl = "https://www.googleapis.com/youtube/v3";
 const youtube = google.youtube({
     version: "v3",
     auth: apiKey,
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-app.get("/search", async (req, res, next) => {
-    try {
-        const searchQuery = req.query.search_query;
-        const url = `${apiUrl}/search?key=${apiKey}&type=video&part=snippet&q=${searchQuery}`;
-
-        const response = await axios.get(url);
-        const titles = response.data.items.map((item) => item.snippet.title);
-
-        res.send(titles);
-    } catch (err) {
-        next(err);
-    }
-});
-
 app.get("/search-with-googleapis", async (req, res) => {
 try {
-        const searchQuery = req.query.search_query;
         const response = await youtube.videos.list({
             part: ["snippet","statistics"],
             chart: "mostPopular",
-            regionCode: "FR",
-            q: searchQuery,
+            regionCode: "GB",
             maxResults: 2,
         });
     res.send(response.data.items.map((item) => {
