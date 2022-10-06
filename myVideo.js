c9cfd34314a445629f6785c87f27019cdf99b808
koala819 <x.genolhac@gmail.com>
2022-10-06 18:05:52 +0200

    test

--- myVideo.js
+++ myVideo.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { google } = require('googleapis');
 const {response} = require("express");
 
-google.youtube('v3').videos.list({
+/*google.youtube('v3').videos.list({
     key: process.env.YOUTUBE_TOKEN,
-    part: ['snippet','statistics','contentDetails','status'],
+    part: ['statistics'],
     id: '8vAdAxgX2FM'
 }).then((response) => {
     const {data} = response;
@@ -12,8 +12,33 @@ google.youtube('v3').videos.list({
        for (const eachSnippet of Object.entries(item)) {
             console.log(eachSnippet);
         };
-        data.items.forEach((item) => {
-            console.log(`Tags: ${item.snippet.tags}\n defaultLanguage: ${item.snippet.defaultLanguage}\n`);
-        })
+       /!*data.items.forEach((item) => {
+            for (const eachThumbnail of Object.entries(item.snippet.thumbnails.default)) {
+                console.log("thumbnails",eachThumbnail);
+            };
+        })*!/
+        /!*data.items.forEach((item) => {
+           console.log(`content_rating_cnc_rating: ${item.contentRating.contentRating.cncRating}\n `);
+       })*!/
+
     })
+}).catch((err) => console.log(err))*/
+
+google.youtube('v3').channels.list({
+    key: process.env.YOUTUBE_TOKEN,
+    //part: ['snippet','contentDetails','statistics','topicDetails','status','brandingSettings','auditDetails','contentOwnerDetails','localizations'],
+    part: ['statistics'],
+    id: 'UCOVAQ6qqaZGsZ6A5XAcFIZg'
+}).then((response) => {
+    const {data} = response;
+    data.items.forEach((item) => {
+        for (const eachItem of Object.entries(item)) {
+            console.log("localizations",eachItem);
+        }
+        ;
+        data.items.forEach((item) => {
+            console.log(`localizations: ${item.localizations}\n `);
+
+        })
+    });
 }).catch((err) => console.log(err))
--- server.js
+++ server.js
@@ -1,57 +1,125 @@
-const express = require("express");
-const axios = require("axios");
-const {google} = require("googleapis");
+var fs = require('fs');
+var readline = require('readline');
+var {google} = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
 
-const app = express();
-const port = 3000;
-const apiKey = "AIzaSyDgk2ql0rVQ5EE6Ayr_I8UlPTRtli26KY8";
-const apiUrl = "https://www.googleapis.com/youtube/v3";
-const youtube = google.youtube({
-    version: "v3",
-    auth: apiKey,
-});
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/youtube-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-app.get("/search", async (req, res, next) => {
-    try {
-        const searchQuery = req.query.search_query;
-        const url = `${apiUrl}/search?key=${apiKey}&type=video&part=snippet&q=${searchQuery}`;
-
-        const response = await axios.get(url);
-        const titles = response.data.items.map((item) => item.snippet.title);
-
-        res.send(titles);
-    } catch (err) {
-        next(err);
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
     }
+    // Authorize a client with the loaded credentials, then call the YouTube API.
+    authorize(JSON.parse(content), getChannel);
 });
 
-app.get("/search-with-googleapis", async (req, res) => {
-    try {
-        const searchQuery = req.query.search_query;
-        const response = await youtube.videos.list({
-            part: ["snippet","statistics"],
-            chart: "mostPopular",
-            regionCode: "FR",
-            q: searchQuery,
-            maxResults: 2,
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    var clientSecret = credentials.web.client_secret;
+    var clientId = credentials.web.client_id;
+    var redirectUrl = credentials.web.redirect_uris[0];
+    var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function(code) {
+        rl.close();
+        oauth2Client.getToken(code, function(err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
         });
-        res.send(response.data.items.map((item) => {
-            return {
-                title: item.snippet.title,
-                description: item.snippet.description,
-                publishedAt: item.snippet.publishedAt,
-                videoId: item.id.videoId,
-                viewCount: item.statistics.viewCount,
-            }}))
-    } catch (err) {
-        console.log ('error with API :: ', err);
-    }
-})
+    });
+}
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) throw err;
+        console.log('Token stored to ' + TOKEN_PATH);
+    });
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function getChannel(auth) {
+    var service = google.youtube('v3');
+    service.channels.list({
+        auth: auth,
+        part: 'snippet,contentDetails,statistics',
+        forUsername: 'GoogleDevelopers'
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var channels = response.data.items;
+        if (channels.length == 0) {
+            console.log('No channel found.');
+        } else {
+            console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
+                'it has %s views.',
+                channels[0].id,
+                channels[0].snippet.title,
+                channels[0].statistics.viewCount);
+        }
+    });
+}
